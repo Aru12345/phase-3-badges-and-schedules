6d7924595cf42a057ebd09a4597d0fb3199d2467
Arundhati <arundhatiraicar12@gmail.com>
2022-01-31 02:21:30 +0530

    commit tree e6d37433843a35386f40d2cff669a51ecfd0a914

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+require "pry"
+def badge_maker(x)
+    "Hello, my name is #{x}."
+end
+
+def batch_badge_creator(array)
+  array.map do |arr|
+    "Hello, my name is #{arr}."
+  end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker,room_num|
+        "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    end
+
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendee|
+        puts attendee
+    end
+    assign_rooms(attendees).each do |attendee|
+        puts attendee
+    end
+    #binding.pry
+
+end
